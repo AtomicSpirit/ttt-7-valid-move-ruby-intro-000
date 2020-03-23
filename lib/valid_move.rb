94d29fe81c4a66bdb871ff9d0cbcdf12bef0d664
Daniel Park <park.daniel1@gmail.com>
2020-03-23 19:29:39 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,6 +7,7 @@ def valid_move?(board, index)
     end
   end
 
+
   def on_board?(num)
     if num.between?(0, 8) == true
       return true
@@ -15,11 +16,10 @@ def valid_move?(board, index)
     end
   end
 
+
   if (position_taken?(board, index)) == false && (on_board?(index) == true)
     return true
   else
     return false
   end
-
-
 end
